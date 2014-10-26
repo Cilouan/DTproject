5e5f9aaa58a354233c7fed0b564e8f15c8ce8018
Konstantinos Fronistas <konfron@yahoo.com>
2014-10-26 18:47:44 +0200

    Simple correction

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
 	public static void main(String [] Args) {
 		System.out.println("\n\nHello World!!!\n\n");
 		Trigamous test = new Trigamous();
-		System.out.println("Trigamous returns: " + Trigamous.getBenorth() + "/n");
+		System.out.println("Trigamous returns: " + Trigamous.getBenorth() + "\n");
 	}
 }
