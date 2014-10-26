beb9fabb01cd0d25e8d5443400d15aa7edbd59a5
Konstantinos Fronistas <konfron@yahoo.com>
2014-10-26 18:43:53 +0200

    Created and printed the object test in HelloWorld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
 	public static void main(String [] Args) {
 		System.out.println("\n\nHello World!!!\n\n");
+		Trigamous test = new Trigamous();
+		System.out.println("Trigamous returns: " + Trigamous.getBenorth() + "/n");
 	}
 }
